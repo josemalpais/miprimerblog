889dabd2f8a6e137f1100d9b1eae76cda9bdb6f7
joseantonio <jose.malpais@hotmail.es>
2019-05-30 12:13:46 +0100

    Mostrar base de datos en server Aythami

--- Apps/views.py
+++ Apps/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def Apps(request):
-	return render(request, 'Apps/post_list.html', {})
+	posts=Post.objects.all().order_by('created_date')
+	return render(request, 'Apps/post_list.html', {'posts':posts})
+
